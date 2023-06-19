febe35d6a3d8db8f70b66dea52755a4e24b7a6ca
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-06-19 08:15:03 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   app/config/coneccion.php
    	new file:   app/config/config.php
    	deleted:    index.htm
    	deleted:    php/login.php
    	new file:   public/index.html

---
+++ app/config/coneccion.php
@@ -0,0 +1,29 @@
+<?php
+ class BDConnection{
+    //creamos el atributo de la clase coneccion para poder instanciar
+    private $connection;
+
+    //definimos el constructor de la clase conection 
+    public function __construct(){
+        //llmamos a una instancia de la configuracion
+        require_once('./config.php');
+        //creqamos la coneccion a la base de datos 
+        $this->connection = new mysqli(BD_HOST, BD_USER, BD_PASSWORD, DB_NAME);
+        //manejo de errores en caso de que no se logre la coneccion 
+        if($this->connection->connect_error)
+        {
+            die('Error de coneccion a la base de datos : '.$this->connection->connect->error)
+        }
+    }
+
+    //creamos el metodo para el momento en que querramos abrir la coneccion a la BD
+    public function getConecction(){
+        return $this->connection;
+    }
+
+    //Creamos el metodo para cerrar la coneccion a la base de datos
+    public function closeConnection(){
+        $this->connection->close();
+    }
+ }
+?>
---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    //definimos cosntantes para la configuracion a la base de datos
+    define('BD_HOST','localhost');
+    define('BD_NAME','bdcatando');
+    define('BD_USER','root');
+    define('BD_PASSWORD','');
+?>
