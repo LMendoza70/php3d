05b8832f10ad2820efdcad316e09cef8110991b1
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-06-21 08:21:38 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   app/model/UserModel.php
    	new file:   app/view/loginView.html
    
     Untracked files:
    	app/controller/

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -25,8 +25,63 @@
             //creamos un arreglo para almacenar cada uno de los usuarios obtenidos de la consulta
             $users=array();
             //creamos el resultado de la consulta
-            
+            while($user =$result->fetch_assoc()){
+                $users[]=$user;
+            }
+            //ya que tenemos los valores de la consulta en una variable que podemos manipular 
+            //cerramos la coneccion 
+            $bdconnection->closeConnection();
+            //retornamos el resultado de la consulta (ya manipulable )
+            return $users;          
         }
 
+        public function getById($id){
+            //paso 1 creamos la la consulta
+                $sql='SELECT * FROM user WHERE IdUser= $id';
+            //paso 2 conectamos con la base de datos
+                $connection=$this->bdconnection->getConnection();
+            //paso 3 ejecutamos la consulta
+                $result =$connection->query($sql);
+            //paso 4 preparar la respuesta 
+            //verificamos si el id pasado como parametro trajo alguna respuedsta
+                if($result && $result->num_rows > 0){
+                    $user = $result->fetch_assoc();
+                } else{
+                    $user=false;
+                }
+            //paso 5 cerrar la coneccion
+                $bdconnection->closeConnection();
+            //paso 6 arrojar el resultado listo 
+                return $user;
+        }
+
+        //metodo que nos permita validar
+        public function getCredentials($user,$password){
+            //paso 1 creamos la la consulta
+                $sql='SELECT * FROM user WHERE Usuario == $user AND Password == $password';
+            //paso 2 conectamos con la base de datos
+                $connection=$this->bdconnection->getConnection();
+            //paso 3 ejecutamos la consulta
+                $result =$connection->query($sql);
+            //paso 4 preparar la respuesta 
+            //verificamos si el id pasado como parametro trajo alguna respuedsta
+                if($result && $result->num_rows > 0){
+                    $user = $result->fetch_assoc();
+                } else{
+                    $user=false;
+                }
+            //paso 5 cerrar la coneccion
+                $bdconnection->closeConnection();
+            //paso 6 arrojar el resultado listo 
+                return $user;
+        }
+
+        //metodo para insertar usuarios 
+        
+        //metodo para eliminar usuarios 
+
+        //metodo para actualizar 
+
+        
     }
 ?>
